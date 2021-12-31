2c66ba6bb0f0fa0cefc15095b05bcbc3297b1b37
PavloBojko <pavlobojko75@gmail.com>
2022-01-01 01:14:22 +0200

    task_12

---
+++ task_12_handler.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if (isset($_POST['add_messege'])) {
+    $add_messege = $_POST['add_messege'];
+    // $_SESSION['$message'][] = 'zbzcvbcvbzcvbzv';
+    $_SESSION['addmessege'][] = $add_messege;
+}
+header('location: /marlin/task_12.php');
+?>
